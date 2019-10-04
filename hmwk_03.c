83206b08b788c6a2a56a257baf585054618db2df
Kanyon Loyd <43161994+KanyonTheManyon@users.noreply.github.com>
2019-10-04 13:59:18 -0500

    Add files via upload

---
+++ hmwk_03.c
@@ -0,0 +1,11 @@
+// Loyd, Kanyon W.
+// kwl7925
+// 2019-10-04
+
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+  printf("Hello, World!\n");
+  return 0;
+}
---
+++ hmwk_03.cc
@@ -0,0 +1,12 @@
+// Loyd, Kanyon W.
+// kwl7925
+// 2019-10-04
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char *argv[] )
+{
+  cout << "Hello, World!" << endl;
+}
